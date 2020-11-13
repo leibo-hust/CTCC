159c47f937235a05b1b5481bb71403f3061b52d8
leibo-hust <leibo@hust.edu.cn>
2020-11-13 11:16:45 +0800

    new main test

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,10 @@
 using namespace std;
 
 int main() {
-	//string filename = "bitmap_test";
-	//string filename = "mvm_test";
-	//string filename = "slt_d";
-	//string filename = "bitmap_de";
-	//string filename = "slt_d";
-	//string filename = "mvmde";
-	//string filename = "mmm_d";
-	string filename = "mmm_de";
-	//string filename = "test_range_var";
-	//string filename = "slt_d_var";
-	//string filename = "test_rangevar";
-
-	readFile(filename+".ll");
+	
+	string filename = "mvm";
+	readFile("../IR/" + filename + ".ll");
 	write_pattern(filename, patternlist);
-	//listtest();
-
 
 	return 0;
 }
