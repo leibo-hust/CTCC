73311cc84520a3a0f259f61e72da0ed5b557725e
leibo-hust <leibo@hust.edu.cn>
2022-05-13 23:09:02 +0800

    new header

--- rcct.h
+++ rcct.h
@@ -5,7 +5,7 @@
 void cim_bitmap(int opcode, int* a, int* b, int* c, int n);
 
 // a[1][M] * B[M][N] = c[1][N];
-void cim_mvm(int M, int N, float* a, float* B, float* c);
+void cim_mvm(int M, int N, float alpha, float beta, float* a, float* B, float* c);
 
 // A[M][N] * B[N][K] = C[M][K];
-void cim_mmm(int M, int N, int K, float* A, float* B, float* C);
+void cim_mmm(int M, int N, int K, float alpha, float beta, float* A, float* B, float* C);
